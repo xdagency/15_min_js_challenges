f3bb42e71586b38fdfc6dd2be06ae9338e8d0341
xdagency <pandabear00001@outlook.com>
2018-07-23 13:38:08 -0400

    Updated morse code challenge with comments

--- Morse Code/morse-code.js
+++ Morse Code/morse-code.js
@@ -13,23 +13,35 @@ of a word. Return the number of different transformations among all words we hav
 
 function morseCode(words) {
 
+    // morse code alphabet
     let mc = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+    // empty array to hold words converted to morse code
     let mcWords = [];
 
+    // first loop through the array of words
     for (let i = 0; i < words.length; i++) {
 
+        // split the word into letters
         let word = words[i].split('');
+        // empty variable to hold the morse code ver of the word
         let mcWord = '';
 
+        // then loop through the letters of the word and concatonate the morse codes
         for (let j = 0; j < word.length; j++) {
+            // 97 is the charcode for a, so whatever the charcode of the letter - 97 will produce the position
+            // in morse code array for that letter
             mcWord = mcWord + mc[word[j].charCodeAt() - 97];
         }
 
+        // push the morse code word to array created earlier
         mcWords.push(mcWord);
 
     }
 
+    // use the spread and Set object to get only unique values
+    // the length of this new array will represent how many unique transformations there are in the array
     let totalTransfoms = [... new Set(mcWords)].length;
+    
     return totalTransfoms;
 
 }
